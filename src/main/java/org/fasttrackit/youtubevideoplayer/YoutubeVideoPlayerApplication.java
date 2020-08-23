5946db2caaa78721d0366c8710f22a3777c7d22a
RaulDobos <raulpriam@yahoo.com>
2020-08-23 15:32:57 +0300

    Initial commit (project structure)

---
+++ src/main/java/org/fasttrackit/youtubevideoplayer/YoutubeVideoPlayerApplication.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.youtubevideoplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeVideoPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeVideoPlayerApplication.class, args);
+    }
+
+}
