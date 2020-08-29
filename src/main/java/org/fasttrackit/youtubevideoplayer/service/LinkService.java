fed1f004b2e9129fef12a41cd534184babe62403
RaulDobos <raulpriam@yahoo.com>
2020-08-29 09:44:11 +0300

    added logger in link service

--- src/main/java/org/fasttrackit/youtubevideoplayer/service/LinkService.java
+++ src/main/java/org/fasttrackit/youtubevideoplayer/service/LinkService.java
@@ -3,11 +3,15 @@ package org.fasttrackit.youtubevideoplayer.service;
 import org.fasttrackit.youtubevideoplayer.domain.Link;
 import org.fasttrackit.youtubevideoplayer.persistence.LinkRepository;
 import org.fasttrackit.youtubevideoplayer.transfer.SaveLinkRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LinkService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LinkService.class);
 
     private final LinkRepository linkRepository;
 
@@ -17,8 +21,7 @@ public class LinkService {
     }
 
     public Link createLink(SaveLinkRequest request){
-        //todo replace with logger
-        System.out.println("Creating link: " + request);
+        LOGGER.info("Creating link {}", request);
         Link link = new Link();
         link.setLink(request.getLink());
         link.setTitle(request.getTitle());
