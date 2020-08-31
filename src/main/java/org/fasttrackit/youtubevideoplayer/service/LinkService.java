f54f492d61c28fd1d2bfcb40d3687697acc2c58a
RaulDobos <raulpriam@yahoo.com>
2020-08-31 18:36:49 +0300

    added get link by id method

--- src/main/java/org/fasttrackit/youtubevideoplayer/service/LinkService.java
+++ src/main/java/org/fasttrackit/youtubevideoplayer/service/LinkService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.youtubevideoplayer.service;
 
 import org.fasttrackit.youtubevideoplayer.domain.Link;
+import org.fasttrackit.youtubevideoplayer.exception.ResourceNotFoundException;
 import org.fasttrackit.youtubevideoplayer.persistence.LinkRepository;
 import org.fasttrackit.youtubevideoplayer.transfer.SaveLinkRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LinkService {
@@ -28,7 +31,32 @@ public class LinkService {
         link.setTitle(request.getTitle());
         link.setImageUrl(request.getImageUrl());
         link.setTime(request.getTime());
-
         return linkRepository.save(link);
     }
+
+    public Link getLink(long id){
+        LOGGER.info("retrieving link {}", id);
+        Optional<Link> linkOptional = linkRepository.findById(id);
+
+//        if(linkOptional.isPresent()){
+//            return linkOptional.get();
+//        }
+//        else{
+//            throw new ResourceNotFoundException("Link " + id + " not found.");
+//        }
+
+        return linkRepository.findById(id)
+                 // Lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException("Link " + id + " not found."));
+    }
+
+
 }
+
+
+
+
+
+
+
+
--- src/test/java/org/fasttrackit/youtubevideoplayer/LinkServiceIntegrationTests.java
+++ src/test/java/org/fasttrackit/youtubevideoplayer/LinkServiceIntegrationTests.java
@@ -1,8 +1,10 @@
 package org.fasttrackit.youtubevideoplayer;
 
 import org.fasttrackit.youtubevideoplayer.domain.Link;
+import org.fasttrackit.youtubevideoplayer.exception.ResourceNotFoundException;
 import org.fasttrackit.youtubevideoplayer.service.LinkService;
 import org.fasttrackit.youtubevideoplayer.transfer.SaveLinkRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,43 @@ class LinkServiceIntegrationTests {
 
     @Test
     void createLink_whenValidRequest_thenReturnCreatedLink() {
+        createLink();
+    }
+
+
+    @Test
+    void createLink_whenMissingMandatoryProperties_thenThrowException(){
+        SaveLinkRequest request = new SaveLinkRequest();
+
+        try {
+            linkService.createLink(request);
+        } catch (Exception e) {
+            assertThat("Unexpected exception thrown", e instanceof ConstraintViolationException);
+        }
+    }
+
+    @Test
+    void getLink_whenExistingLink_thenReturnLink(){
+        Link link = createLink();
+
+        Link response = linkService.getLink(link.getId());
+
+        assertThat(response, notNullValue());
+        assertThat(response.getId(), greaterThan(0L));
+        assertThat(response.getLink(), is(link.getLink()));
+        assertThat(response.getTitle(), is(link.getTitle()));
+        assertThat(response.getTime(), is(link.getTime()));
+
+    }
+
+    @Test
+    void getLink_whenNonExistingLink_thenThrowResourceNotFoundException(){
+        Assertions.assertThrows(ResourceNotFoundException.class,
+                () -> linkService.getLink(0));
+
+    }
+
+    private Link createLink() {
         SaveLinkRequest request = new SaveLinkRequest();
         request.setLink("https://www.youtube.com/watch?v=L_jWHffIx5E");
         request.setTitle("Smash Mouth - All Star");
@@ -36,18 +75,10 @@ class LinkServiceIntegrationTests {
         assertThat(link.getLink(), is(request.getLink()));
         assertThat(link.getTitle(), is(request.getTitle()));
         assertThat(link.getTime(), is(request.getTime()));
+
+        return link;
     }
 
-    @Test
-    void createProduct_whenMissingMandatoryProperties_thenThrowException(){
-        SaveLinkRequest request = new SaveLinkRequest();
-
-        try {
-            linkService.createLink(request);
-        } catch (Exception e) {
-            assertThat("Unexpected exception thrown", e instanceof ConstraintViolationException);
-        }
-    }
 
 }
 
